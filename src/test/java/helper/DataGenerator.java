85b155c74402df8f215e7302ccc535b9ccbf7561
tiger154 <junhwen@naver.com>
2019-03-31 17:27:05 +0900

    1. Data generator refactoring to guarantee max limit array   - 데이터제너레이터 리팩토링(최대 배열에 맞춘 데이터생성 검증)

--- src/test/java/helper/DataGenerator.java
+++ src/test/java/helper/DataGenerator.java
@@ -1,17 +1,34 @@
 package helper;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class DataGenerator {
 
-
+    /**
+     * Make sure it return max limit array
+     * @param min
+     * @param max
+     * @return
+     */
     public static int[] randomRangedInts(int min, int max) {
-        int [] arr = new Random()
-                    .ints(max * 10, min, max+1)
-                    .distinct()
-                    .limit(max)
-                    .toArray();
+        int [] arr;
+        do {
+           arr = DataGenerator.randomRangedIntsNotGuaranteeLenth(min, max);
+        } while (arr.length < max);
         return arr;
+    }
+
+    /**
+     * Base random ranged number generator, but not guarantee to make max limit array
+     * @param min
+     * @param max
+     * @return
+     */
+    private static int[] randomRangedIntsNotGuaranteeLenth(int min, int max) {
+        return new Random()
+                .ints(max * 10, min, max+1)
+                .distinct()
+                .limit(max)
+                .toArray();
     }
 }
