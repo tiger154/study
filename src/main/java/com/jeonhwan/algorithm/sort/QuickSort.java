2fe66a5ebc497b860d88a040187a3a5f79c3675a
tiger154 <junhwen@naver.com>
2019-04-06 19:28:06 +0900

    1. QuickSort algorithm implement    퀵소트 알고리즘 구현 2. Generate better test data    테스트 데이터 제너레이팅 기능 향상

--- src/main/java/com/jeonhwan/algorithm/sort/QuickSort.java
+++ src/main/java/com/jeonhwan/algorithm/sort/QuickSort.java
@@ -1,4 +1,104 @@
 package com.jeonhwan.algorithm.sort;
 
 public class QuickSort {
+
+    private int[] data;
+
+    public QuickSort(int[] data) {
+        this.data = data;
+    }
+
+
+    /**
+     * I implemented myself. It takes over 4hours with debugging
+     * It was actually really hard to figure out without check reference.
+     *
+     *
+     * Problem
+     *   - After it swap between bigger_left and smaller_right, I check from start element again
+     *     It should be changed as swap point should be next compare point.
+     *
+     * Mistake Note
+     *
+     *  - Didn't set the while's exit condition
+     *  - My big problem was without debugging couldn't imagine myself for recursive  case deeply
+     *  - Tried to do without first/last index, it means I was confused if should use reference data or separate array and merge
+     *    Of course it should be referenced data control.
+     *  - Wasn't sure what init values for bigger_left_idx/smaller_right_idx
+     *    and How to handle if it fail to find bigger_left_idx.
+     *    1) For init values I set [bigger_left_idx = first_idx], [smaller_right_idx = last_idx]
+     *    2) To handle if there is not bigger value then pivot I set fake bigger value(last_index+1)
+     *       to trigger pivot swap
+     *  - Compare point should be bigger_left/smaller_right but I tried first_index and last_index
+     *    which lead dramatic speed change.
+     *
+     *
+     * @param data
+     * @param first_idx
+     * @param last_idx
+     * @return
+     */
+    public int[] sortJeonhwanStyle(int [] data, int first_idx, int last_idx) {
+        if (first_idx == data.length -1) {
+            return data;
+        }
+
+        // 데이터의 길이가 1 이하인 경우 즉각 loop 를 빠져나간다.
+        if (last_idx - first_idx < 1) {
+            return null;
+        }
+
+        int pivot = data[first_idx];
+        int last = data[last_idx];  // Not using last value so need deleted
+        int bigger_left_idx = first_idx;
+        int smaller_right_idx = last_idx;
+
+
+        while (bigger_left_idx < smaller_right_idx) {
+            // # 1. find bigger(left -> right)
+            for (int i = bigger_left_idx + 1; i < last_idx+1; i++) {
+                if (pivot < data[i]) {
+                    bigger_left_idx = i;
+                    break;
+                }
+            }
+            // # 2. find smaller(right -> left)
+            for(int i = smaller_right_idx; i >= first_idx ; i--) {
+                if(pivot >= data[i]) {
+                    smaller_right_idx = i;
+                    break;
+                }
+            }
+            // # 3. if failed to find bigger value then pivot, set fake bigger value
+            if (first_idx == bigger_left_idx) {
+                bigger_left_idx = last_idx + 1;
+            }
+
+            // if stagger happened swap pivot with smaller_right_index
+            // And do sort for both side left/right
+            if (bigger_left_idx > smaller_right_idx) {
+                // swap and call quick sort again
+                int temp = data[smaller_right_idx];
+                data[smaller_right_idx] = data[first_idx];
+                data[first_idx] = temp;
+
+                // start left quickSort
+                sortJeonhwanStyle(data, first_idx,smaller_right_idx-1);
+                // start right quickSort
+                sortJeonhwanStyle(data,smaller_right_idx+1, last_idx);
+
+            } else {
+                // swap [here after swap... we can ]
+                int temp = data[bigger_left_idx];
+                data[bigger_left_idx] = data[smaller_right_idx];
+                data[smaller_right_idx] = temp;
+            }
+        }
+        return data;
+    }
+
+
+
+
+
 }
--- src/test/java/algorithm/sort/QuickSortTest.java
+++ src/test/java/algorithm/sort/QuickSortTest.java
@@ -1,8 +1,10 @@
 package algorithm.sort;
 
 import com.jeonhwan.algorithm.sort.BubbleSort;
-import com.jeonhwan.algorithm.sort.SelectionSort;
+import com.jeonhwan.algorithm.sort.InsertionSort;
+import com.jeonhwan.algorithm.sort.QuickSort;
 import helper.DataGenerator;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -10,11 +12,11 @@ import java.util.stream.IntStream;
 
 import static org.junit.Assert.assertArrayEquals;
 
-public class BubbleSortTest {
+public class QuickSortTest {
 
 
     @Test
-    public void given_100_random_data_bubble_sort_asc_Test() {
+    public void given_100_random_data_quick_sort_asc_Test() {
         int[] expected_data = IntStream.iterate(1, n -> n + 1).limit(100).toArray();
         int[] result_data;
 
@@ -24,9 +26,8 @@ public class BubbleSortTest {
             // copy array to track before sorted
             int[] temp = Arrays.copyOf(input_data, input_data.length);
 
-            BubbleSort bubbleSort = new BubbleSort(input_data);
-            result_data = bubbleSort.sort();
-
+            QuickSort quickSort = new QuickSort(input_data);
+            result_data = quickSort.sortJeonhwanStyle(input_data, 0, input_data.length -1);
             System.out.println("input_data:" + Arrays.toString(temp) + ", result_data: " + Arrays.toString(result_data));
 
             // # Assert
@@ -35,13 +36,18 @@ public class BubbleSortTest {
     }
 
 
-
     /**
-     * Big O(N^2)
+     * Big O(N logN)
      *
-    */
+     *
+     * Data scenario
+     *
+     *  [5,2,6,9,8,7,10,1,3,4] 0
+     *
+     */
+    @Ignore
     @Test
-    public void basic_bubble_sort_asc_test() {
+    public void basic_quick_sort_asc_test() {
 
         // given
         int[] input_data = {5,2,6,9,8,7,10,1,3,4};
@@ -50,8 +56,8 @@ public class BubbleSortTest {
         int[] temp = Arrays.copyOf(input_data, input_data.length);
 
         // act
-        BubbleSort bubbleSort= new BubbleSort(input_data);
-        result_data = bubbleSort.sort();
+        QuickSort quickSort = new QuickSort(input_data);
+        result_data = quickSort.sortJeonhwanStyle(input_data, 0, input_data.length - 1);
 
         System.out.println("input_data:" + Arrays.toString(temp) + ", result_data: " + Arrays.toString(result_data));
 
@@ -59,5 +65,9 @@ public class BubbleSortTest {
         assertArrayEquals(expected_data, result_data);
 
     }
+
+
+
+
 
 }
