f9e4440564344b194e7d2a4fd6ab32a6abb01afe
jeonhwan <jeonhwan.won@cj.net>
2020-03-09 13:00:40 +0900

    Java Substring test added

--- src/test/java/BasicTest.java
+++ src/test/java/BasicTest.java
@@ -21,6 +21,42 @@ public class BasicTest {
             square_result = square_result * target_num;
             log.debug("square {}^{} result is: {}", target_num , i+1 , NumberFormat.getInstance().format(square_result));
         }
+    }
 
+    @Test
+    public void substring_index_0_test() {
+        String str = "hi there";
+
+        String sub0 = str.substring(0,0);
+        String sub1 = str.substring(0,1);
+        String sub2 = str.substring(0,2);
+        String sub3 = str.substring(1,4);
+
+        //quick brown fox jumps over the lazy dog
+        log.debug("sub0: {}, sub1: {}, sub2: {}, sub3: {}", sub0 ,sub1, sub2, sub3);
+    }
+
+
+    /**
+     * Left index start from 0
+     * Right index start from 1
+     */
+    @Test
+    public void substring_index_pattern_test() {
+        String str = "abcabcde";
+
+
+        String sub0 = str.substring(0,1);
+        String sub1 = str.substring(1,2);
+
+        String sub2 = str.substring(0,2);
+        String sub3 = str.substring(2,4);
+
+        String sub4 = str.substring(0,3);
+        String sub5 = str.substring(3,6);
+
+        //quick brown fox jumps over the lazy dog
+        log.debug("sub0: {}, sub1: {}", sub0 ,sub1);
+        log.debug("sub2: {}, sub3: {}", sub2 ,sub3);
     }
 }
