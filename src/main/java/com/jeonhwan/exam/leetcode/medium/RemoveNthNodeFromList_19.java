5f16e0a64a7046aca04c268b9a90e12404a725b9
jeonhwan <jeonhwan.won@cj.net>
2020-07-21 13:33:35 +0900

    Code clean

---
+++ src/main/java/com/jeonhwan/exam/leetcode/medium/RemoveNthNodeFromList_19.java
@@ -0,0 +1,91 @@
+package com.jeonhwan.exam.leetcode.medium;
+
+import java.util.LinkedList;
+
+public class RemoveNthNodeFromList_19 {
+
+    public static class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode() {}
+        public ListNode(int val) { this.val = val; }
+        public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    /**
+     *  I designed myself, and I learned Java pass by value in depth.
+     *    1) Copy ListNode to Queue : N time
+     *    2) Copy back to ListNode(Removed Nth Node): N Time
+     *
+     *   - Big O: O(n*2)
+     *
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode brute_force(ListNode head, int n) {
+
+        // 1. copy data to flat = N time
+        LinkedList<Integer> temp_queue = new LinkedList<>();
+        temp_queue.add(head.val);
+
+        ListNode temp_next = head.next;
+        while (temp_next != null) {
+            temp_queue.add(temp_next.val);
+            temp_next = temp_next.next;
+        }
+
+
+        // 2. copy data as linked list
+
+        // now lets think about again for practicing
+        // 1. Need to set linked-list in loop of temp_queue
+        //  1) Set first element as head!
+        //  2) Set temp_next element to use new next argument
+        //  3) Start loop
+        //    3.1) Make a temp instance and use                 --> Save address of this instance
+        //    3.2) Set next.next = temp_instance
+        //    3.3) In the end set next = temp_instance
+        //  * Alright now Im gonna pus skip logic here!
+
+        // needed removed index
+        int skip_index = temp_queue.size()-n;
+        // if remove index is 0 then just pop and go through
+        if(skip_index == 0) {
+            temp_queue.pop();
+        }
+
+        // If it's empty then return
+        if(temp_queue.size() == 0) {
+            return null;
+        }
+
+        // init first data for head
+        ListNode head_rtn = new ListNode(temp_queue.pop());
+        ListNode next = null; // it doesn't matter null or any garbage instance
+
+        // loop
+        int index = 1;
+        while (temp_queue.size() > 0) {
+            // 1. pop first data
+            ListNode temp_instance = new ListNode(temp_queue.pop());
+            // 2. Skip if it hit skip index
+            if (skip_index == index) {
+                index++;
+                continue;
+            }
+            // 3. Set next data
+            if (head_rtn.next == null) {
+                head_rtn.next = temp_instance;
+            } else {
+                next.next = temp_instance;
+            }
+            // 4. Set next for next iteration
+            next = temp_instance;
+            // 5. index increment
+            index++;
+        }
+
+        return head_rtn;
+    }
+}
---
+++ src/test/java/exam/leetcode/medium/RemoveNthNodeFromList_19Test.java
@@ -0,0 +1,39 @@
+package exam.leetcode.medium;
+
+import com.jeonhwan.algorithm.sort.MergeSort;
+import com.jeonhwan.exam.leetcode.medium.RemoveNthNodeFromList_19;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.NodeList;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+
+public class RemoveNthNodeFromList_19Test {
+    private static Logger log = LoggerFactory.getLogger(MergeSort.class);
+
+
+
+
+    @Test
+    public void first_test() {
+
+        RemoveNthNodeFromList_19.ListNode head = new RemoveNthNodeFromList_19.ListNode(1);
+        head.next = new RemoveNthNodeFromList_19.ListNode(2);
+        head.next.next = new RemoveNthNodeFromList_19.ListNode(3);
+        head.next.next.next = new RemoveNthNodeFromList_19.ListNode(4);
+        head.next.next.next.next = new RemoveNthNodeFromList_19.ListNode(5);
+
+
+        RemoveNthNodeFromList_19 solution = new RemoveNthNodeFromList_19();
+        RemoveNthNodeFromList_19.ListNode result = solution.brute_force(head, 2);
+
+       log.debug("Lets test!");
+
+    }
+
+
+}
