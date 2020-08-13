4ffef1bd832d42846e81cc4963d1cc38c775394f
jeonhwan <jeonhwan.won@cj.net>
2020-08-13 14:35:19 +0900

    multithread test   - multiple url request and return the data   - Compared multiple new instance with multi thread vs using one instance with multithread
        - if you use one instance and use 'synchronized method block', then it's almost same as not using multi-thread
          because even though you use multi thread they need to wait one by one while others using it.
    
        - If you create new instance every time on Runnable task, then it has more freedom to run the method
          If lock level is 'this' or 'synchronized method level', but you need to make sure in the synchronized block
          if there is some 'shared value' you need to make sure if that is thread safe. let say if there is an int type shared value,
          and multi thread access, you should make that int to AtomicInteger type as it's thread safe.
          If you are not sure using that value as thread safe then may you can create new class which has thread safe properties.
          And you can use make an instance of this and when control the properties put synchronized block then you can make sure
          those shared values are thread safe. but of course you need to test as always!

---
+++ src/test/java/play/MultiThreadRequesterTest.java
@@ -0,0 +1,273 @@
+package play;
+
+import com.jeonhwan.algorithm.sort.MergeSort;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Make simple request call and log result with multi thread
+ *
+ * make sample url list
+ * run using ExecutorService(threadPool)
+ *
+ * one instance only. many function call
+ * If generate too many
+ *
+ */
+public class MultiThreadRequesterTest {
+
+    private static Logger log = LoggerFactory.getLogger(MultiThreadRequesterTest.class);
+
+
+
+
+    @Test
+    public void isValidUrls() throws MalformedURLException, IOException {
+
+        long startTime = System.nanoTime();
+        List<ResponseModel> list = Collections.synchronizedList(new ArrayList<ResponseModel>()); // multi threads gonna access to this value, so it must be thread safe
+//        List<ResponseModel> remote_list = list;
+        // 1. get urls
+        List<String> urls = new ArrayList<>();
+        urls.add("https://play.google.com/store/apps/details?id=com.bestappfactory.lotto_1");
+        urls.add("https://play.google.com/store/apps/details?id=com.eyefilter.night");
+        urls.add("https://play.google.com/store/apps/details?id=com.quick.clean.easy");
+        urls.add("https://play.google.com/store/apps/details?id=com.cashwalk.cashwalk");
+
+
+        int threadPoolSize = 5;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(threadPoolSize);
+
+        // 2. loop urls
+        for (String url : urls) {
+            // lets do lambda here
+            Runnable task = () -> {
+                try {
+                    // make every time new instance for safety
+                    Requester requester =  new Requester();
+                    requester.setList(list);
+                    requester.requestAndCodeReturn(url);
+                } catch (Exception e) {
+                   System.out.println(e.getMessage());
+                }
+            };
+            executorService.submit(task);
+        }
+
+        executorService.shutdown();
+        try {
+            if(!executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS)) {
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            log.debug(e.getMessage());
+            executorService.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+
+
+
+
+       // 3. run
+//       Requester requester =  new Requester();
+//       int res_code =  requester.requestAndCodeReturn("https://play.google.com/store/apps/details?id=com.bestappfactory.lotto_1");
+
+       long elapsedTime = System.nanoTime() - startTime;
+
+        System.out.println("Total execution time in millis: " + elapsedTime/1000000 + ", in seconds: " + TimeUnit.MILLISECONDS.toSeconds(elapsedTime/1000000));
+
+       log.debug("hi there");
+    }
+
+
+    @Test
+    public void isValidUrls_brute_force() throws MalformedURLException, IOException {
+
+        long startTime = System.nanoTime();
+
+        // 1. get urls
+        List<String> urls = new ArrayList<>();
+        urls.add("https://play.google.com/store/apps/details?id=com.bestappfactory.lotto_1");
+        urls.add("https://play.google.com/store/apps/details?id=com.eyefilter.night");
+        urls.add("https://play.google.com/store/apps/details?id=com.quick.clean.easy");
+        urls.add("https://play.google.com/store/apps/details?id=com.cashwalk.cashwalk");
+
+
+        int threadPoolSize = 5;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(threadPoolSize);
+
+        // 2. loop urls
+        for (String url : urls) {
+            Requester requester =  new Requester();
+            requester.requestAndCodeReturn(url);
+        }
+
+        long elapsedTime = System.nanoTime() - startTime;
+
+        System.out.println("Total execution time in millis: " + elapsedTime/1000000);
+
+        log.debug("hi there");
+    }
+
+
+    public class ResponseModel {
+        private int res_code;
+        private String url;
+
+        public int getRes_code() {
+            return res_code;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setRes_code(int res_code) {
+            this.res_code = res_code;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+    }
+
+
+
+    public class Requester {
+
+        private String url;
+        private String return_code;
+        private List<ResponseModel> list;
+        private Object lock = new Object();
+
+        public Requester() {}
+
+        public Requester(String url, String return_code, List<ResponseModel> list) {
+            this.url = url;
+            this.return_code = return_code;
+            this.list = list;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public String getReturn_code() {
+            return return_code;
+        }
+
+        public List<ResponseModel> getList() {
+            return list;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public void setReturn_code(String return_code) {
+            this.return_code = return_code;
+        }
+
+        public void setList(List<ResponseModel> list) {
+            this.list = list;
+        }
+
+        /**
+         * Make a request and return code
+         * Also we can control duration here (Must be some singleton duration checker needed)
+         *
+         * @param url_string
+         * @return
+         * @throws MalformedURLException
+         * @throws IOException
+         */
+        public int requestAndCodeReturn(String url_string) throws MalformedURLException, IOException {
+            synchronized (lock) {
+                URL url =  new URL(url_string);
+                HttpURLConnection con = (HttpURLConnection)url.openConnection();
+                con.setRequestMethod("GET");
+                int res_code = con.getResponseCode();
+
+                // log here may we can put down the result to a file..
+                System.out.println(Thread.currentThread().getName() + ": res_code=" + res_code + ", url: " + url_string);
+
+                // insert result as well
+                // this not gonna be safe is not thread safe
+                ResponseModel rm =  new ResponseModel();
+                rm.setRes_code(res_code);
+                rm.setUrl(url_string);
+                list.add(rm);
+
+
+                return res_code;
+            }
+        }
+    }
+
+
+
+
+    public class Counter implements Runnable {
+        public int counter = 0;
+        public final int limit = 10;
+        public final Object lock = new Object();
+        @Override
+        public void run() {
+            while (counter < limit) {
+                increaseCounter();
+            }
+        }
+        private void increaseCounter() {
+            synchronized (lock) {
+                //log.debug("hi there");
+                // log.debug("count: {}", counter);
+               // log.debug("{} : {}", Thread.currentThread().getName(), 1);
+                System.out.println(Thread.currentThread().getName() + " : " + counter);
+                counter++;
+           }
+        }
+    }
+
+
+    /**
+     * So If we use one instance to multi thread, we need to lock
+     * But if it's every time new instance, it's working well
+     */
+    @Test
+    public void count_one_instance_with_multithread_test() {
+
+        int threadPoolSize = 2;
+        Counter counter = new Counter();
+        ExecutorService executorService = Executors.newFixedThreadPool(threadPoolSize);
+        for (int i =0; i < threadPoolSize; i++) {
+            executorService.submit(counter);
+        }
+         executorService.shutdown();
+    }
+
+    @Test
+    public void count_multi_instance_with_multithread_test() {
+
+        int threadPoolSize = 3;
+        ExecutorService executorService = Executors.newFixedThreadPool(threadPoolSize);
+        for (int i =0; i < threadPoolSize; i++) {
+            executorService.submit(new Counter());
+        }
+         executorService.shutdown();
+    }
+}
