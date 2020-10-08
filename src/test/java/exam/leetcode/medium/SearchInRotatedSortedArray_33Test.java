8e9850b31e0f7e803b771511b7e94ef8c5997065
tiger154 <demoprogrammer@gmail.com>
2020-10-08 18:31:42 +0900

    Prepared test class for LeetCode 33th problem

---
+++ src/test/java/exam/leetcode/medium/SearchInRotatedSortedArray_33Test.java
@@ -0,0 +1,30 @@
+package exam.leetcode.medium;
+
+import org.junit.Test;
+
+public class SearchInRotatedSortedArray_33Test {
+
+
+
+    @Test
+    public void solution_test1() {
+
+    }
+
+
+    /**
+     * 1. Rotating mean from random index position, it swap left and right.
+     *
+     *
+     * @return
+     */
+    public int first_approach() {
+
+
+
+        return 1;
+    }
+
+
+
+}
